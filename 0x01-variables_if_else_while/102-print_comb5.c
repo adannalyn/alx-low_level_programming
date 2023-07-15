a9174823fc5ecb4fc18ddfdb4a067b2025cadf34
adannalyn <qvexvon@gmail.com>
2023-07-14 20:57:08 -0700

    c programming

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,19 +9,13 @@ int main(void)
 
 	for (count1 = 0; count1 < 99; count1++)
 	{
-		for (count2 = 0; count2 < 100; count2++)
+		for (count2 = count1; count2 < 100; count2++)
 		{
-			if (count1 >= 10)
-			{
-				putchar((count1 / 10) + '0');
-				putchar((count1 % 10) + '0');
-			}
+			putchar((count1 / 10) + '0');
+			putchar((count1 % 10) + '0');
 			putchar(' ');
-			if (count2 >= 10)
-			{
-				putchar((count2 / 10) + '0');
-				putchar((count2 % 10) + '0');
-			}
+			putchar((count2 / 10) + '0');
+			putchar((count2 % 10) + '0');
 			putchar (',');
 			putchar (' ');
 			if (count1 == count2)
