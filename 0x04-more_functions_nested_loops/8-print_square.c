9ada2ef8da01b0375ff08eae0ad30f08a3da47dd
adannalyn <qvexvon@gmail.com>
2023-07-22 07:13:49 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,14 @@ void print_square(int size)
 {
 	int count1, count2;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 	for (count1 = 0; count1 < size; count1++)
 	{
 		for (count2 = 0; count2 < size; count2++)
 		{
-			if (size <= 0)
-			{
-				_putchar('\n');
-			}
 			_putchar('#');
 		}
 		_putchar('\n');
