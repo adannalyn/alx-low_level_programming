6e5ff5753975945bf1cab67ffa3d4d46db4e0140
adannalyn <qvexvon@gmail.com>
2023-07-22 06:44:30 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,5 +6,14 @@
  */
 int main(void)
 {
-    
+	int prime;
+
+	for (prime = 0; prime < 1231952; prime++)
+	{
+		if (1231952 % prime)
+		{
+			printf("%d", prime);
+		}
+	}
+	return (prime);
 }
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of square
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int count1, count2;
+
+	for (count1 = 0; count1 < size; count1++)
+	{
+		for (count2 = 0; count2 < size; count2++)
+		{
+			if (size <= 0)
+			{
+				_putchar('\n');
+			}
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
