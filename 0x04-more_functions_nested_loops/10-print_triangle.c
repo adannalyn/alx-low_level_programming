63816b76c4dbcee4cdfbab15a315da0f79ad53e6
adannalyn <qvexvon@gmail.com>
2023-07-23 17:04:48 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,25 @@ void print_triangle(int size)
 {
 	int count1, count2, space;
 
-	for (count1 = 0; count1 <= size; count1++)
+	if (size <= 0)
 	{
-		if (size <= 0)
-		{
-			_putchar('\n');
-		}
-
+		_putchar('\n');
+	}
+	for (count1 = 0; count1 < size; count1++)
+	{
 		for (space = size; space > count1; space--)
 		{
+			if (size == count1)
+			{
+				break;
+			}
 			_putchar(' ');
 		}
-		for (count2 = 0; count2 < count1; count2++)
+		for (count2 = 0; count2 <= count1; count2++)
 		{
 			_putchar('#');
 		}
 		_putchar('\n');
 	}
+
 }
