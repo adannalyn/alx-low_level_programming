ec82408099bcb1b043c282d169a1d828f9299956
adannalyn <qvexvon@gmail.com>
2023-08-05 14:29:47 +0100

    commandline

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,9 @@
-// #ifndef MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 int _putchar(char c);
+void print_string(char *str);
 int main(int argc, char *argv[]);
-// #endif MAIN_H
+
+#endif MAIN_H
