47d677d160be586fcf2b799799a1c5700631a73f
adannalyn <qvexvon@gmail.com>
2023-08-05 14:26:44 +0100

    commandline arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,35 @@
 #include "main.h"
 /**
+ * print_string - print string
+ * @str: string
+*/
+void print_string(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
+
+
+/**
  * main - entry point
  * @argc: count
  * @argv: pointer
  * Return: 0
-*/
+ */
 int main(int argc, char *argv[])
 {
-int count;
+	int count;
 
-if (argc > 0)
-{
-for (count = 0; count < argc; count++)
-{
-_putchar(*argv[count]);
-}
-_putchar('\n');
-}
-return (0);
+	if (argc > 0)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			print_string(argv[count]);
+		}
+		_putchar('\n');
+	}
+	return (0);
 }
