f4610e2882d0af53b7c13f197ce067520e97bf03
adannalyn <qvexvon@gmail.com>
2023-08-07 11:55:32 +0100

    commands

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: pointer
+*/
 int main(int argc, __attribute__((unused)) char *argv[])
 {
-    putchar(argc - 1 + '0');
-    putchar('\n');
+	putchar(argc - 1 + '0');
+	putchar('\n');
 	return (0);
 }
