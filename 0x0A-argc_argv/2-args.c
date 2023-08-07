ea2e577f013d984355876ef3e1e9871301769843
adannalyn <qvexvon@gmail.com>
2023-08-07 14:21:27 +0100

    commands

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,30 +1,34 @@
 #include "main.h"
 /**
+ * print_string - print string
+ * @str: string
+ */
+void print_string(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+}
+
+/**
  * main - this is the entry point
  * @argc: count
  * @argv: pointer
  * Return: 0
  */
-void print_string(char *str)
-{
-    while (*str != '\0')
-    {
-        putchar(*str);
-        str++;
-    }
-}
-
 int main(int argc, char *argv[])
 {
-    int count;
+	int count;
 
-    if (argc)
-    {
-	    for (count = 0; count < argc; count++)
-	    {
-		    print_string(argv[count]);
-            putchar('\n');
-	    }
-    }
-    return (0);
+	if (argc)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			print_string(argv[count]);
+			putchar('\n');
+		}
+	}
+	return (0);
 }
