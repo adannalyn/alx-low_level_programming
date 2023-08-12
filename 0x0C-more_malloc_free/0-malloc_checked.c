bbca9324732861387c4bd3ee72f429c7a9c68bf6
adannalyn <qvexvon@gmail.com>
2023-08-12 15:33:51 +0100

    static

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+  int *c = 0;
+   :     int *d = 0;
+        c = calloc(b,sizeof (int));
+        d = malloc(b: * sizof(int);
+        if (c = NULL)
+        {
+                return (NULL);
+        }
+        else
+        {
+                return (98);
+        }
+		   }
