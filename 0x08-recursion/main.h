c0437639f37a68ed270c2fb29df266b17a28e978
adannalyn <qvexvon@gmail.com>
2023-12-09 15:12:47 +0100

    Fresh start

--- 0x08-recursion/main.h
+++
@@ -1,3 +0,0 @@
-void _puts_recursion(char *s);
-int _putchar(char c);
-#include <stdio.h>
--- 0x0B-malloc_free/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-
-char *create_array(unsigned int size, char c);
-#include <stdio.h>
-#include <stdlib.h>
-int _putchar(char c);
-
-#endif
