3194287531a3bdff092f475f4e67e7853a375e2f
adannalyn <qvexvon@gmail.com>
2024-01-19 03:11:25 +0100

    Fresh start

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,1 +1,18 @@
-int _isupper(int c){return (0);}
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ * @c: character
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
