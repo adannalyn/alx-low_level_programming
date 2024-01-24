43766c94f29aea5b3114cc24f88e91e4afab5e64
adannalyn <qvexvon@gmail.com>
2024-01-24 07:35:01 +0100

    print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,23 +1,21 @@
 #include "dog.h"
 
-
 /**
  * print_dog - print detail of dog
  * @d: struct
  */
 
-
 void print_dog(struct dog *d)
 {
-	char *name;
-	float age;
-	char *owner;
-
 	if (d == NULL)
 	{
 		printf("nil");
 	}
-
-	d = malloc(sizeof(d) * );
-
+	else if (d->name == NULL)
+	{
+		printf("(Name: (nil)");
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+
+/**
+ * new_dog - create new dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *ptr;
+	ptr = &dog_t;
+
+	ptr->name;
+}
