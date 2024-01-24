d1237b6661cdb38499fc607d452fd2fc9a5c53fa
adannalyn <qvexvon@gmail.com>
2024-01-24 05:37:43 +0100

    dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+
+/**
+ * print_dog - print detail of dog
+ * @d: struct
+ */
+
+
+void print_dog(struct dog *d)
+{
+	char *name;
+	float age;
+	char *owner;
+
+	if (d == NULL)
+	{
+		printf("nil");
+	}
+
+	d = malloc(sizeof(d) * );
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,15 +12,15 @@ int _putchar(char c);
  * @owner: third member
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 
 
 #endif
