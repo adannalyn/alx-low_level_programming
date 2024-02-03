65790dd1516e2867ccf68d0110f7a302c6403dfd
adannalyn <qvexvon@gmail.com>
2024-02-03 10:15:36 -0800

    Link test1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,18 +8,26 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-
-	new_node = malloc(sizeof(list_t));
+/*
+ *	if (head == NULL || str == NULL)
+ *	{
+ *		return (NULL);
+ *	}
+ */	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	else
-	{
-		return (new_node);
-	}
+
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	return (0);
+	return (new_node);
 }
