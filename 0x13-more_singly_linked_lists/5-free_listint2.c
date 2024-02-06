bd1802671170721896bfd679e6837933833f7c23
adannalyn <qvexvon@gmail.com>
2024-02-06 14:02:42 -0800

    singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,17 @@ void free_listint2(listint_t **head)
 {
 	listint_t *new_node;
 
-	while (*head != NULL)
+	while (*head)
 	{
-		new_node = *head;
-		*head = (*head)->next;
-		free(new_node);
+		if (*head != NULL)
+		{
+			new_node = *head;
+			*head = (*head)->next;
+			free(new_node);
+		}
+		else if (*head == NULL)
+		{
+			free(head);
+		}
 	}
 }
