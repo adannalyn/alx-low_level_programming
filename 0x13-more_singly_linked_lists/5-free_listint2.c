0fe50c1f0573e0945af7bad577402bad969be93c
adannalyn <qvexvon@gmail.com>
2024-02-06 13:23:41 -0800

    singly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head: pointer to head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new_node;
+
+	while (*head != NULL)
+	{
+		new_node = *head;
+		*head = (*head)->next;
+		free(new_node);
+	}
+}
