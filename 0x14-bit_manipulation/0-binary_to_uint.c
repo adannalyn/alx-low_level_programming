36264ba5fc7b068fa35dfd8bb2e116546ad4e963
adannalyn <qvexvon@gmail.com>
2024-02-10 11:57:10 -0800

    binary to integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert a binary number to unsigned int
+ * @b: pointer to binary
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int count = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b == '0')
+		{
+			count = count * 2;
+		}
+		else if (*b == '1')
+		{
+			count = count * 2 + 1;
+		}
+		else
+		{
+			return (0);
+		}
+		b++;
+	}
+	return (count);
+}
