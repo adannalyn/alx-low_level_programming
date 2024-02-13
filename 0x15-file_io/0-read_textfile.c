263d4679c35a61242190ea000c7850e1f0f1772b
adannalyn <qvexvon@gmail.com>
2024-02-13 14:00:22 -0800

    files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * read_textfile - read from a file
+ * @filename: name of file
+ * @letters:
+ * Return:
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	if (file == NULL)
+	{
+		return (0);
+	}
+
+	fd = open("filename", 0_RDWR | 0_CREAT);
+	
+}
