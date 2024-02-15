95540591b68019bf78df6ba53f1fd5a8864461f9
adannalyn <qvexvon@gmail.com>
2024-02-15 03:01:42 -0800

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,11 +7,12 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buf[100];
-	int count = 100;
-	int fd;
-	ssize_t reed;
+	char *buf;
+	size_t count = 100;
+	int fd, fe;
+	ssize_t reed, wraite;
 
+	buf = malloc(sizeof(char *) * count);
 	if (filename == NULL)
 	{
 		return (0);
@@ -22,12 +23,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	reed = read(fd, buf, count);
-	while (reed != '\0')
+	while (reed != 0)
 	{
-		printf("%ld", reed);
-	       reed++;
+		if (reed == -1)
+			return (-1);
+		printf("%s", buf);
 	}
-	write(fd, buf, count);
-	close(fd);
+	wraite = write(fd, buf, count);
+	if (wraite == -1)
+		return (-1);
+	printf("%s",buf);
+	fe = close(fd);
+	if (fe == -1)
+		return (-1);
 	return (letters);
+
 }
