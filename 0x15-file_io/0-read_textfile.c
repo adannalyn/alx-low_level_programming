fbbe47b632bcbbe144668ce4bc783937c52d4c87
adannalyn <qvexvon@gmail.com>
2024-02-15 00:15:19 -0800

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,12 +7,27 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char buf[100];
+	int count = 100;
 	int fd;
-	if (file == NULL)
+	ssize_t reed;
+
+	if (filename == NULL)
 	{
 		return (0);
 	}
-
-	fd = open("filename", 0_RDWR | 0_CREAT);
-	
+	fd = open(filename, O_RDWR, letters);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	reed = read(fd, buf, count);
+	while (reed != '\0')
+	{
+		printf("%ld", reed);
+	       reed++;
+	}
+	write(fd, buf, count);
+	close(fd);
+	return (letters);
 }
