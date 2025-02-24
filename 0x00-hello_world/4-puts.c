f1dfbb3469b1312168b1a76b5cd10b508f01c8d4
adannalyn <qvexvon@gmail.com>
2025-02-24 00:25:36 -0800

    RE: Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**
- * main - entry point
- * Return: 0
- */
-int main(void)
+
+int main()
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
