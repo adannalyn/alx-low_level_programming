21f1b9fe5d76724ba54e8ef24aa922fe79d79185
adannalyn <qvexvon@gmail.com>
2025-02-26 00:36:37 -0800

    SE: LowerCase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char lower_case;
 
-	for (lower_case = 'a'; lower_case < 'z'; lower_case++)
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 		putchar(lower_case);
 	putchar('\n');
 	return (0);
