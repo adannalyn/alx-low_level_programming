328fb3a72d8c851e39fd9fd2b02e6cb737408b8f
adannalyn <qvexvon@gmail.com>
2025-02-26 04:11:45 -0800

    SE: LowerCase Reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'z'; lower_case >= 'a'; lower_case--)
+		putchar(lower_case);
+	putchar('\n');
+	return (0);
+}
