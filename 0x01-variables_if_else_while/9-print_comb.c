b4b5b0abaaeae63c6e88d36180c216841c3a1665
adannalyn <qvexvon@gmail.com>
2025-02-26 05:20:04 -0800

    SE: Commas and Spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+		if (number == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
