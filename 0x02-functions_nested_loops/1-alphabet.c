489cfc6f52fb746f868f22b5ffe43297c0b5058a
adannalyn <qvexvon@gmail.com>
2025-03-11 10:35:38 -0700

    SE: Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet: prints the alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
