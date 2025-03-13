d5a299372984a80874bc5eb6f3320662c8b81673
adannalyn <qvexvon@gmail.com>
2025-03-13 00:20:53 -0700

    SE: islower

---
+++ 0x02-functions_nested_loops/hello.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - checks for lowercase character
+ * Return: 0
+ */
+int main(int alpha)
+{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		return (alpha);
+	putchar('\n');
+	return 0;
+}
