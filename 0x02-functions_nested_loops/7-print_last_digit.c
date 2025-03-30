10ec4ff1e7d23a645434c4090ed648315a5e9ebe
adannalyn <qvexvon@gmail.com>
2025-03-30 04:36:31 -0700

    SE: Print Last Number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,33 +4,19 @@
  * @n: integer
  * Return: 0
  */
-void print_number(int num)
-{
-	int digit, divisor = 1;
-
-	if (num == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	if (num < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-	while (num / divisor >= 10)
-		divisor *= 10;
-	while (divisor != 0)
-	{
-		digit = (num / divisor) % 10;
-		_putchar(digit + '0');
-		divisor /= 10;
-	}
-}
 int print_last_digit(int n)
 {
-	int rem;
-	rem = n % 10;
-	print_number(rem);
+	int remDigit;
+
+	remDigit = n % 10;
+	if (remDigit < 0)
+	{
+		remDigit = -remDigit;
+		_putchar('0' + remDigit);
+	}
+	if (remDigit > 0)
+	{
+		_putchar('0' + remDigit);
+	}
 	return (0);
 }
