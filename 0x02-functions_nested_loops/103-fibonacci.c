ebe12faacda79d4ffa1c06bcaf6a1a52c084b6c3
adannalyn <qvexvon@gmail.com>
2025-04-13 06:05:26 -0700

    SE: Fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,21 +5,19 @@
  */
 int main(void)
 {
-	unsigned long count, fibo = 0, first = 2, second = 3, length = 32;
+	unsigned long count, first = 2, second = 3, fibo = 0, result = 0, length = 32;
 
 	for (count = 0; count <= length; count++)
 	{
 		if (first % 2 == 0)
-			printf("%lu", first);
+		{
+			result += first;
+		}
 		fibo = first + second;
 		first = second;
 		second = fibo;
-		if (count != length || first % 2 != 0)
-		{
-			putchar(',');
-			putchar(' ');
-		}
 	}
+	printf("%lu", result);
 	putchar('\n');
 	return (0);
 }
