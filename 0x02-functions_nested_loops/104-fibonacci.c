f7766660c86082be07e691975bab83b24b9f4eb3
adannalyn <qvexvon@gmail.com>
2025-04-25 22:49:43 -0700

    SE: Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,20 +5,29 @@
  */
 int main(void)
 {
-	unsigned long count, fibo = 0, first = 1, second = 2, length = 98;
+	unsigned long first = 1, second = 2;
+	unsigned long third = 0, fourth = 0;
+	int count, length = 98;
+	unsigned long temp_low, temp_high;
 
-	for (count = 1; count <= length; count++)
+	printf("1, 2");;
+	for (count = 2; count <= length; count++)
 	{
-		printf("%lu", first);
-		fibo = first + second;
-		first = second;
-		second = fibo;
-
-		if (count != length)
+		temp_low = first + second;
+		temp_high = third + fourth;
+		if (temp_low >= 1000000000)
 		{
-			putchar(',');
-			putchar(' ');
+			temp_low -= 1000000000;
+			temp_high += 1;
 		}
+		if (temp_high > 0)
+			printf(", %lu", temp_low);
+		else
+			printf(", %lu%09lu", temp_high, temp_low);
+		first = second;
+		second = temp_low;
+		third = fourth;
+		fourth = temp_high;
 	}
 	putchar('\n');
 	return (0);
