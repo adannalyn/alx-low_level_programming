87dfaed9d8757fc1512f95a528a47e0bcae4d28f
adannalyn <qvexvon@gmail.com>
2025-04-30 10:47:01 -0700

    SE: Debugging 3

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -29,6 +29,14 @@ int main(void)
     day = convert_day(month, day);
     print_remaining_days(month, day, year);
 
+    month = 02;
+    day = 29;
+    year = 2001;
+
+    printf("Date: %02d/%02d/%04d\n", month, day, year);
+    day = convert_day(month, day);
+    print_remaining_days(month, day, year);
+
     month = 03;
     day = 01;
     year = 2009;
