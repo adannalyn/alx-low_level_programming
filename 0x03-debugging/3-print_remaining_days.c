25f275957297e4d19dc589e6606535ddb5ce0ee7
adannalyn <qvexvon@gmail.com>
2025-04-30 10:49:18 -0700

    SE: Debugging 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if (year % 4 == 0 || year % 400 == 0)
 	{
-		if ((month > 0 && month < 13) || (day > 0 && day < 32))
+		if (month < 13 || day < 32)
 		{
 			day++;
 		}
