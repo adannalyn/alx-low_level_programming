ed3c5c3c8d7c5d661593ad0d5818c8954c6bf902
adannalyn <qvexvon@gmail.com>
2025-05-08 15:49:34 -0700

    SE: Digits

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		if (!(number == 2 || number == 4))
+			_putchar(number + '0');
+	_putchar('\n');
+}
