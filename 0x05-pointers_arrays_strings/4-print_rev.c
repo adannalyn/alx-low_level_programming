a58bb96404d5fb93d07141fe39ab915aa96a9ad0
adannalyn <qvexvon@gmail.com>
2025-08-11 13:29:14 -0700

    SE: reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,10 +3,18 @@
  * print_rev - print in reverse
  * @s: string
  */
+int _strlen(char *str) {
+  int count;
+
+  while (*str != '\0') {
+    count++;
+  }
+  return (count);
+}
 void print_rev(char *s) {
   int len, count;
 
-  len = strlen(s);
+  len = _strlen(s);
   for (count = len - 1; count >= 0; count--) {
     _putchar(s[count]);
   }
