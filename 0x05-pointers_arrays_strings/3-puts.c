8b29d9a687e8554b9530275656775d0f914781d1
adannalyn <qvexvon@gmail.com>
2025-08-11 02:53:18 -0700

    SE: puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,11 @@
  * _puts - prints a string
  * @str: string
  */
-void _puts(char *str)
-{
-	
+void _puts(char *str) {
+  int count;
+
+  for (count = 0; count < _strlen(str); count++) {
+    _putchar(str[count]);
+  }
+  _putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
-
-
 
 #endif
