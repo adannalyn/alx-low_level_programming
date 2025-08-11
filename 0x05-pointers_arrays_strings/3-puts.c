e3a1704d07301ff75108587cce32b2d64ba37346
adannalyn <qvexvon@gmail.com>
2025-08-11 03:07:59 -0700

    SE: puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,13 @@
  * _puts - prints a string
  * @str: string
  */
-void _puts(char *str) {
-  int count;
+void _puts(char *str)
+{
+	int count;
 
-  for (count = 0; count < _strlen(str); count++) {
-    _putchar(str[count]);
-  }
-  _putchar('\n');
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		putchar(str[count]);
+	}
+	_putchar('\n');
 }
