745d3a908c5de0710b3f1a97842df8b8157ab142
adannalyn <qvexvon@gmail.com>
2025-10-03 04:08:37 -0700

    SE: Print an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - prints an array
+ * @a: integer
+ * @n: integer
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	printf("%d", a[0]);
+
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", a[i]);
+	}
+
+	printf("\n");
+}
