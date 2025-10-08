02d3c15273f31b6eb9c1e568d3db8778efc77dc5
adannalyn <qvexvon@gmail.com>
2025-10-08 00:34:19 -0700

    SE: String to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _atoi - string to integer
+ * @s: string
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int started = 0;
+	int digit;
+
+	while (*s)
+	{
+		if (*s == '-' && !started)
+			sign *= -1;
+		else if (*s == '+' && !started)
+		{
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			digit = *s - '0';
+
+			if (result > INT_MAX / 10 ||
+			    (result == INT_MAX / 10 && digit > INT_MAX % 10))
+			{
+				return (sign == 1) ? INT_MAX : INT_MIN;
+			}
+			result = result * 10 + digit;
+		}
+		else if (started)
+			break;
+		s++;
+	}
+	return (result * sign);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
@@ -13,7 +14,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
+int _atoi(char *s);
 
 
 #endif
