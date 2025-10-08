b90c39f1ba1b36dc08848854fa430e89867555e7
adannalyn <qvexvon@gmail.com>
2025-10-08 10:58:41 -0700

    SE: to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
  * *string_toupper - change letters to uppercase
- *
+ * @str: character
  * Return: Always 0.
  */
 char *string_toupper(char *str)
 {
-	while (str)
+	int count = 0;
+
+	while (str[count] != '\0')
 	{
-		if (*str >= 'a' || *str <= 'z')
-			_putchar(*str - 32);
-		str++;
+		if (str[count] >= 'a' && str[count] <= 'z')
+			str[count] = str[count] - 32;
+		count++;
 	}
-	return (0);
+	return (str);
 }
